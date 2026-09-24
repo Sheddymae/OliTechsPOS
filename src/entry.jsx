94bd291f461d308b6c90da4eab95acfc56fd4e62
Sheddymae <125443020+Sheddymae@users.noreply.github.com>
2026-09-24 04:45:23 +0300

    Fix application route to use complete App component

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './main.jsx';
+import App from './App.jsx';
 import { PublicLanding, PublicSignup } from './public-site.jsx';
 
 const path = window.location.pathname;
